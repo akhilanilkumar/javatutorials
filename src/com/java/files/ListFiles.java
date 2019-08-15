edccafe94c5af207b31e44cadaf116bc3a25607f
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2019-08-15 23:23:48 +0530

    File & Folder lists

--- src/com/java/files/ListFiles.java
+++ src/com/java/files/ListFiles.java
@@ -1,8 +1,30 @@
 package com.java.files;
 
+import java.io.File;
+
 public class ListFiles {
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        //System.out.println(factorial(5));
+        final String PATH = "E:\\Media\\Movies";
+        File file = new File(PATH);
+        if (file.exists() && file.isDirectory()) {
+            recursiveFiles(file.listFiles(), 0, 0);
+        }
+    }
+
+    private static void recursiveFiles(File[] files, int index, int level) {
+        if (files.length == index) {
+            return;
+        }
+        for (int i = 0; i < level; i++) {
+            System.out.print("\t");
+        }
+        File file = files[index];
+        System.out.println(file.getName());
+        if (file.isDirectory()) {
+            recursiveFiles(file.listFiles(), 0, level + 1);
+        }
+        recursiveFiles(files, ++index, level);
     }
 
     private static int factorial(int number) {
